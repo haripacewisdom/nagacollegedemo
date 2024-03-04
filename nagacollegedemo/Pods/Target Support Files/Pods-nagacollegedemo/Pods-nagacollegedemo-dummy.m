c20f5efcb2cc827f385103a156c79c78930c2d3f
hariharaprabu.m <hariharaprabu.m@pacewisdom.com>
2024-03-04 13:28:04 +0530

    init commit

---
+++ nagacollegedemo/Pods/Target Support Files/Pods-nagacollegedemo/Pods-nagacollegedemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_nagacollegedemo : NSObject
+@end
+@implementation PodsDummy_Pods_nagacollegedemo
+@end
---
+++ nagacollegedemo/nagacollegedemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nagacollegedemo
+//
+//  Created by User on 04/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
